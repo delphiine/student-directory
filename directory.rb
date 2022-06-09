9800cddfcf41e86e80f0e4c384f91d9ea9354e6b
delphine <delphine.mubiligi@gmail.com>
2022-06-09 23:22:48 +0100

    added a method for an interactive menu

--- directory.rb
+++ directory.rb
@@ -5,9 +5,20 @@ def input_students
   students = []
   name = gets.chomp
   
-  while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+  until name.empty? do 
+    
+    puts "Whats #{name}'s hobby?"
+    hobby = gets.chomp
+    puts "wWats your cohort?"
+    cohort = gets.chomp.to_sym 
+    students << {name: name, hobby: hobby, cohort: cohort}
+
+    if students.length != 1
+      puts "Now we have #{students.count} students - add a name or hit enter to exit."
+    else
+      puts "Now we have 1 student - add a name or hit enter to exit."
+    end
+
     name = gets.chomp
   end
   students
@@ -20,7 +31,7 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{student[:name]}'s hobby is #{student[:hobby]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -28,6 +39,32 @@ def print_footer(students)
   puts "overall, we have  #{students.count} great students"
 end
 
+def interactive_menu
+  students = []
+  loop do
+  # 1. print the menu and ask the user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more items
+  # 2. read the input and save it into a variable
+  selection = gets.chomp
+  # 3. do what the user has asked
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit # this will cause the program to terminate
+    else
+      puts "I don't know what meant, try again"
+    end
+  end
+end
+
+interactive_menu
 students = input_students
 print_header
 print(students)
